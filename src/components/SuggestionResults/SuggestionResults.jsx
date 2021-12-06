ea22b609f0eb212d0b7ae7ead24a6622028cee61
RamonM47 <aviramon.mesa@gmail.com>
2021-12-06 16:20:18 -0500

    fix issue with accessing user profile through name

---
+++ src/components/SuggestionResults/SuggestionResults.jsx
@@ -0,0 +1,18 @@
+// import React, { useState } from 'react'
+// import { Link, useNavigate } from 'react-router-dom'
+
+// const suggestionResults = (props) => {
+
+//     const results
+
+//     return ( 
+//         <>
+//             <h2>
+//                 These are some {} suggestions
+//             </h2>
+//         </>
+
+//     );
+// }
+
+// export default suggestionResults
